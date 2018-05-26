7da15aa6e1299131bb6bad3adabc85df1f68239f
Paul Wicking <wowsuchnamaste@protonmail.com>
2018-05-26 16:38:46 +0200

    Refactor tests
    Change test that Person can have name to Person _must_ have name.

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -1,16 +1,15 @@
-from address_book import address_book
+from address_book.address_book import Person
 from unittest import main, TestCase
 
 
 class TestAddressBook(TestCase):
     def setUp(self):
         self.test_person_name = "Bob Smith"
-        self.test_person = address_book.Person(name=self.test_person_name)
+        self.test_person = Person(name=self.test_person_name)
 
-    def test_person_can_have_a_name(self):
-        result = self.test_person.name
-
-        assert result == self.test_person_name
+    def test_person_must_have_a_name(self):
+        with self.assertRaises(TypeError):
+            Person()
 
     def test_person_can_have_an_address(self):
         test_person_address = {'Street 1': 'Some street 1',
@@ -18,14 +17,14 @@ class TestAddressBook(TestCase):
                                'Postal code': 12345,
                                'City': 'Test City',
                                'Country': 'Test Country'}
-        test_person_with_address = address_book.Person(self.test_person_name, test_person_address)
+        test_person_with_address = Person(self.test_person_name, test_person_address)
 
         result = test_person_with_address.address
         assert result == test_person_address
 
     def test_person_can_have_a_phone_number(self):
         test_person_phone_number = "555-1234"
-        test_person_with_phone_number = address_book.Person(
+        test_person_with_phone_number = Person(
             self.test_person_name, phone_number=test_person_phone_number)
 
         result = test_person_with_phone_number.phone_number
