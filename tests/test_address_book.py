574278ba203a170e6c94f080f1b78f19f452b00f
Paul Wicking <wowsuchnamaste@protonmail.com>
2018-05-26 16:21:35 +0200

    Refactor tests
    Add setUp with person object
    Remove superfluous tests (import, instantiate Person)
    Clean up import

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -3,40 +3,32 @@ from unittest import main, TestCase
 
 
 class TestAddressBook(TestCase):
-    def test_can_import_address_book(self):
-        import address_book
-
-    def test_can_create_a_person(self):
-        test_person = address_book.Person('test')
-
-        assert test_person is not None
+    def setUp(self):
+        self.test_person_name = "Bob Smith"
+        self.test_person = address_book.Person(name=self.test_person_name)
 
     def test_person_can_have_a_name(self):
-        test_person_name = "Bob Smith"
-        test_person = address_book.Person(name=test_person_name)
+        result = self.test_person.name
 
-        result = test_person.name
-
-        assert result == test_person_name
+        assert result == self.test_person_name
 
     def test_person_can_have_an_address(self):
-        test_person_name = "Bob Smith"
         test_person_address = {'Street 1': 'Some street 1',
                                'Street 2': 'Some more info',
                                'Postal code': 12345,
                                'City': 'Test City',
                                'Country': 'Test Country'}
-        test_person = address_book.Person(test_person_name, test_person_address)
+        test_person_with_address = address_book.Person(self.test_person_name, test_person_address)
 
-        result = test_person.address
+        result = test_person_with_address.address
         assert result == test_person_address
 
     def test_person_can_have_a_phone_number(self):
-        test_person_name = "Bob Smith"
         test_person_phone_number = "555-1234"
-        test_person = address_book.Person(test_person_name, phone_number=test_person_phone_number)
+        test_person_with_phone_number = address_book.Person(
+            self.test_person_name, phone_number=test_person_phone_number)
 
-        result = test_person.phone_number
+        result = test_person_with_phone_number.phone_number
         assert result == test_person_phone_number
 
 
