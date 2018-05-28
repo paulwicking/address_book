ddc7d82f60a2cd364a3ca7b7644cae4e28383ab7
Paul Wicking <paulwicking@gmail.com>
2018-05-28 17:42:42 +0200

    Refactor: add setUp for address book tests, fix typo

--- tests/test_address_book.py
+++ tests/test_address_book.py
@@ -3,13 +3,12 @@ from address_book.address_book import AddressBook, Person
 
 
 class TestAddressBook(TestCase):
-    def test_can_create_an_address_book(self):
-        address_book = AddressBook()
+    def setUp(self):
+        self.address_book = AddressBook()
 
-        self.assertIsInstance(address_book, AddressBook)
+        self.assertIsInstance(self.address_book, AddressBook)
 
     def test_can_add_entry_to_address_book(self):
-        address_book = AddressBook()
-        test_parson = Person("John Doe")
+        test_person = Person("John Doe")
 
-        address_book.add_entry(test_parson)
+        self.address_book.add_entry(test_person)
